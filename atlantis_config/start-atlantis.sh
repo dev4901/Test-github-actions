e13300172fb51de03ab0348c08c4ba12c0c4ba21
devansh shah <dev4901@outlook.com>
2025-07-16 09:51:02 +0000

    update atlantis yaml file

--- atlantis_config/start-atlantis.sh
+++ atlantis_config/start-atlantis.sh
@@ -9,4 +9,5 @@ atlantis server \
   --repo-allowlist="$REPO_ALLOWLIST" \
   --port="$PORT" \
   --atlantis-url="$ATLANTIS_URL" \
-  --gh-webhook-secret="$WEBHOOK_SECRET"
+  --gh-webhook-secret="$WEBHOOK_SECRET" \
+  --write-git-creds
