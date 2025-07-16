83becd32f1f221ed247bad6cef7740bd3f1e91c1
Devansh Shah <dev4901@outlook.com>
2025-07-16 07:52:42 +0000

    upload atlantis config files

---
+++ atlantis_config/start-atlantis.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+set -e
+
+. ./atlantis.var
+
+atlantis server \
+  --gh-app-id="$GH_APP_ID" \
+  --gh-app-key-file="$GH_APP_KEY_FILE" \
+  --repo-allowlist="$REPO_ALLOWLIST" \
+  --port="$PORT" \
+  --atlantis-url="$ATLANTIS_URL" \
+  --gh-webhook-secret="$WEBHOOK_SECRET"
