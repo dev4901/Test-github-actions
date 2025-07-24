9d75f50ec262f8969961051303551d3514821f91
Devansh Shah <61081870+dev4901@users.noreply.github.com>
2025-07-24 14:09:02 +0530

    Update output.tf

--- terraform-test/gce-new/output.tf
+++ terraform-test/gce-new/output.tf
@@ -4,11 +4,6 @@ output "instance_name" {
   value       = google_compute_instance.web_server.name
 }
 
-output "instance_external_ip" {
-  description = "External IP address of the instance"
-  value       = google_compute_instance.web_server.network_interface[0].access_config[0].nat_ip
-}
-
 output "instance_internal_ip" {
   description = "Internal IP address of the instance"
   value       = google_compute_instance.web_server.network_interface[0].network_ip
