ff1eea51075f8a6f7d1f583749a6a91b9442424a
Devansh Shah <dev4901@outlook.com>
2025-08-25 08:55:32 +0000

    python code update

--- python_test.py
+++ python_test.py
@@ -16,7 +16,7 @@ instance_resource = service.instances().get(
 
 # Labels
 labels = instance_resource.get('labels', {})
-labels['hello-test2'] = 'this-new-label'
+labels['hello-test3'] = 'this-new-label'
 
 # Set new labels
 req = service.instances().setLabels(
