3ebf600e5c553b70fc93e024e08b544f7001bf9c
Devansh Shah <dev4901@outlook.com>
2025-08-25 08:52:55 +0000

    python code update

--- python_test.py
+++ python_test.py
@@ -16,7 +16,7 @@ instance_resource = service.instances().get(
 
 # Labels
 labels = instance_resource.get('labels', {})
-labels['hello-gh-actions'] = 'this-new-label'
+labels['hello-test2'] = 'this-new-label'
 
 # Set new labels
 req = service.instances().setLabels(
